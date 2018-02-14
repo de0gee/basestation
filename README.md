eef49c5ffb56b82c4cee94608953231cd590ff3e
Zack Scholl <zack.scholl@gmail.com>
2018-02-13 18:58:02 -0800

    Update Profile

--- README.md
+++ README.md
@@ -35,3 +35,87 @@ sudo python3 -m pip install gatt
 sudo apt-get install python3-dbus
 python3 run.py
 ```
+
+# BlueSense GATT Profile
+
+```
+Last updated: 2018-FEB-13
+
+Service 1: [Device Information]
+sourceId=org.bluetooth.service.device_information
+uuid = 180A
+    Characteristic 1: [Manufacturer Name String]
+    sourceId = org.bluetooth.characteristic.manufacturer_name_string
+    uuid = 2A29
+    value type =  utf-8
+    length (bytes) = 3
+
+    Characteristic 2: [Model Number String]
+    sourceId = org.bluetooth.characteristic.model_number_string
+    uuid = 2A24
+    value type =  utf-8
+    length (bytes) = 9
+    
+    Characteristic 3: [Hardware Revision String]
+    sourceId = org.bluetooth.characteristic.hardware_revision_string
+    uuid = 2A27
+    value type =  utf-8
+    length (bytes) = 3
+
+    Characteristic 4: [Firmware Revision String]
+    sourceId = org.bluetooth.characteristic.firmware_revision_string
+    uuid = 2A26
+    value type =  utf-8
+    length (bytes) = 5
+
+Service 2: [Battery Service]
+sourceId=org.bluetooth.service.battery_service
+uuid = 180F
+    Characteristic 1: [Battery Level]
+    sourceId = org.bluetooth.characteristic.battery_level
+    uuid = 2A19
+    value type =  uint8_t
+    length (bytes) = 1
+
+Service 3: [Environmental Sensors]
+sourceId=custom.type
+uuid = c355c42e-b56c-458e-bacb-9248717bbac2
+    Characteristic 1: [Temperature]
+    sourceId = org.bluetooth.characteristic.temperature
+    uuid = 2A6E
+    value type =  int16_t
+    length (bytes) = 2
+
+Characteristic 2: [Humidity]
+    sourceId = org.bluetooth.characteristic.humidity
+    uuid = 2A6F
+    value type =  uint8_t
+    length (bytes) = 1
+
+    Characteristic 3: [Ambient Light]
+    sourceId = custom.type
+    uuid = c24229aa-d7e4-4438-a328-c2c548564643
+    value type =  uint32_t
+    length (bytes) = 4
+
+    Characteristic 4: [UV Light]
+    sourceId = custom.type
+    uuid = 61bf1164-529c-4140-9c61-3f5e4fb4c0c1
+    value type =  uint8_t
+    length (bytes) = 1
+
+    Characteristic 5: [Pressure]
+    sourceId = custom.type
+    uuid = 2f256c42-cdef-4378-8e78-694ea0f53ea8
+    value type =  uint16_t
+    length (bytes) = 2
+
+Service 4: [IMU]
+sourceId=custom.type
+uuid = 5b2c25e7-7c43-4a15-a4c6-7cf2d81e1b40
+    Characteristic 1: [Motion]
+    sourceId = custom.type
+    uuid = 15e438b8-558e-4b1f-992f-23f90a8c129b
+    value type =  uint16_t
+    length (bytes) = 2
+```
--- run.py
+++ run.py
@@ -1,7 +1,15 @@
+import struct
+
 import gatt
 
 manager = gatt.DeviceManager(adapter_name='hci0')
 
+characteristic_names = {
+    '15e438b8-558e-4b1f-992f-23f90a8c129b':'motion',
+    '2f256c42-cdef-4378-8e78-694ea0f53ea8':'pressure',
+    'c24229aa-d7e4-4438-a328-c2c548564643':'ambient light'
+
+}
 class AnyDevice(gatt.Device):
     def services_resolved(self):
         super().services_resolved()
@@ -15,7 +23,6 @@ class AnyDevice(gatt.Device):
             c for c in device_information_service.characteristics
             if c.uuid == '00002a29-0000-1000-8000-00805f9b34fb')
 
-        firmware_version_characteristic.enable_notifications()
         firmware_version_characteristic.read_value()
 
 
@@ -28,26 +35,32 @@ class AnyDevice(gatt.Device):
             c for c in device_information_service.characteristics
             if c.uuid == '15e438b8-558e-4b1f-992f-23f90a8c129b')
 
-        firmware_version_characteristic.enable_notifications()
         firmware_version_characteristic.read_value()
 
-        # temperature
+        # ambient light
         device_information_service = next(
             s for s in self.services
             if s.uuid == 'c355c42e-b56c-458e-bacb-9248717bbac2')
 
         firmware_version_characteristic = next(
             c for c in device_information_service.characteristics
-            if c.uuid == '00002a6e-0000-1000-8000-00805f9b34fb')
+            if c.uuid == 'c24229aa-d7e4-4438-a328-c2c548564643')
 
-        firmware_version_characteristic.enable_notifications()
+        # firmware_version_characteristic.enable_notifications()
         firmware_version_characteristic.read_value()
 
     def characteristic_value_updated(self, characteristic, value):
-        print(characteristic.uuid,value[0],value[1])
-        # keep reading values if nessecary
-        if characteristic.uuid in ['15e438b8-558e-4b1f-992f-23f90a8c129b','00002a6e-0000-1000-8000-00805f9b34fb']:
-            characteristic.read_value()
+        if characteristic.uuid not in characteristic_names:
+            pass
+        data = 0
+        if characteristic_names[characteristic.uuid] == 'ambient light':
+            # uint32_t, 4 bytes
+            data =struct.unpack('<L',value)[0]
+        elif  characteristic_names[characteristic.uuid] == 'motion':
+            # uint16_t, 2 bytes
+            data =struct.unpack('<H',value)[0]
+        print(characteristic_names[characteristic.uuid],data)
+        characteristic.read_value()
 
     # def characteristic_enable_notifications_succeeded(self,characteristic,value):
     #     print("HI")
