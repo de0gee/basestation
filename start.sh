b3e7ee4319e8ed69450efd079f25b1833d0ee9d3
Zack Scholl <zack.scholl@gmail.com>
2018-03-13 22:14:26 -0700

    whoops

--- start.sh
+++ start.sh
@@ -10,8 +10,8 @@ sudo service bluetooth restart
 # update the basestation
 cd $GOPATH/src/github.com/de0gee/basestation
 git pull
-curl -s https://api.github.com/repos/de0gee/basestation/releases/latest  | grep 'arm' | grep '.tar.gz' | grep 'http' | cut -d '"' -f 4 | wget -qi -
 rm -rf basestation-linux*
+curl -s https://api.github.com/repos/de0gee/basestation/releases/latest  | grep 'arm' | grep '.tar.gz' | grep 'http' | cut -d '"' -f 4 | wget -qi -
 tar -xvzf basestation-linux-arm-6.tar.gz
 chmod +x basestation-linux-arm-6
 nohup sudo ./basestation-linux-arm-6 >/tmp/server.log 2>&1 &
